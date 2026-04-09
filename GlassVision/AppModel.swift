dbf18d022f144a54ca578aeecd3d5a9ff0c5b369
NaroEngineering <128151988+NaroEngineering@users.noreply.github.com>
2026-04-09 10:22:05 -0400

    Initial Commit

---
+++ GlassVision/AppModel.swift
@@ -0,0 +1,21 @@
+//
+//  AppModel.swift
+//  GlassVision
+//
+//  Created by Max Caro on 4/9/26.
+//
+
+import SwiftUI
+
+/// Maintains app-wide state
+@MainActor
+@Observable
+class AppModel {
+    let immersiveSpaceID = "ImmersiveSpace"
+    enum ImmersiveSpaceState {
+        case closed
+        case inTransition
+        case open
+    }
+    var immersiveSpaceState = ImmersiveSpaceState.closed
+}
---
+++ GlassVisionTests/GlassVisionTests.swift
@@ -0,0 +1,17 @@
+//
+//  GlassVisionTests.swift
+//  GlassVisionTests
+//
+//  Created by Max Caro on 4/9/26.
+//
+
+import Testing
+@testable import GlassVision
+
+struct GlassVisionTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
